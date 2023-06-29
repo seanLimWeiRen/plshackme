d40234354f57f94720ac6f33cd92368eef7f39c7
ChenHaowei2008 <haoweichenchw@gmail.com>
2023-06-29 14:37:15 +0800

    Fixed errors

--- server/users.ts
+++ server/users.ts
@@ -1,5 +1,32 @@
 import prisma from "./database";
 
+export async function getUserByID(findID: string) {
+  return await prisma.user.findUnique({
+    where: {
+      id: findID
+    }
+  })
+}
+
 export async function getAllUsers() {
   return await prisma.user.findMany()
+}
+
+export async function deleteUserByID(deleteID: string){
+  // check if user exists before deleting
+  if(getUserByID(deleteID) == null){
+    return true
+  }
+
+  try{
+    await prisma.user.delete({
+      where: {
+        id: deleteID
+      }
+    })
+    return true
+  } catch (e){
+    console.log(e)
+    return false
+  }
 }
